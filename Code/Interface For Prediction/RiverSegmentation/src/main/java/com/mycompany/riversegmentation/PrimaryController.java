6cf8cba4f07dcd4875cc7b071a7133954ea929bc
MoadSaadKhorchef <32598991+MoadSaadKhorchef@users.noreply.github.com>
2021-08-15 03:46:15 +0100

    Update PrimaryController.java

--- Code/Interface For Prediction/RiverSegmentation/src/main/java/com/mycompany/riversegmentation/PrimaryController.java
+++ Code/Interface For Prediction/RiverSegmentation/src/main/java/com/mycompany/riversegmentation/PrimaryController.java
@@ -1,7 +1,12 @@
 package com.mycompany.riversegmentation;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -46,11 +51,90 @@ public class PrimaryController {
         fileChooser.setTitle(titel);
         return fileChooser.showOpenDialog(new Stage());
     }
-
+    
+    
     @FXML
     void runButtonFunction(ActionEvent event) {
+        
+        try
+        {
+            //imResult =
+            //resultImage.setImage(mat2Image(imgSource));
+            Thread outStreamReader = new Thread(new Runnable() {
+                public void run() {
+
+                    try {
+                        
+                        //ExecutorService executor = Executors.newSingleThreadExecutor();
+                        ProcessBuilder processBuilder = new ProcessBuilder();
+
+                        processBuilder = processBuilder.redirectErrorStream(true);
+
+                        String imgDir = pictureNameTextField.getText();
+
+                        String[] command = {"cmd.exe", "/c", "python.exe" ,"C:\\Users\\keith\\Documents\\NetBeansProjects\\gui\\src\\gui\\Prediction.py",""+imgDir};
+
+                        processBuilder.command(command);
+
+                        processBuilder.directory(new File("C:\\Users\\keith\\Documents\\NetBeansProjects\\gui\\src\\gui\\"));
+
+                        Process process = processBuilder.start();
+                        List<String> alist=new ArrayList<String>();  
+                        List<String> flist=new ArrayList<String>();  
+
+                        String line = null;
+                        BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));                        
+
+                        while ((line = in.readLine()) != null) {
+                            in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                            
+                            alist.add(line);
+                            //flist = alist.stream().filter(l -> l.startsWith("Text")).collect(Collectors.toList());
+                            //flist = in.lines().filter(l -> l.startsWith("confidence")).collect(Collectors.toList());
+
+                            if(!alist.isEmpty()){
+                                //alist.forEach(System.out::println);    
+                                //flist.forEach(cLine -> detectionResultTextArea.setText(cLine));
+                                final String s = alist.get(0);
+                                //int size = alist.size();
+                                //System.out.println(size); 
+                                //Platform.setImplicitExit(false);
+                                //resultfeedback(s);
+                                //Platform.runLater(  () -> detectionResultTextArea.setText(s) )  ;
+                                //Thread.sleep(1000);
+                                line = null;
+                            }  
+
+                        //TimeUnit.SECONDS.sleep(30);   
+                        }
+
+                        int exitCode = process.waitFor();
+                            alist.clear();
+                            flist.clear();
+                        System.out.println("\nExited with error code : " + exitCode);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    //System.out.println("Exit reading process output");
+                }
+            });
+            outStreamReader.start();  
+            
+           // webCamCapture = new WebCamCapture(this.cameraFrame);
+
+                //  String line;                
+                //  int exitCode = process.waitFor();
+                //  System.out.println("\nExited with error code : " + exitCode);
+        }catch (NullPointerException e){
+        
+        }finally{   
+            // resultImage.setImage(mat2Image(imResult));
+            // resultImage.setImage(mat2Image(imgSource));
+        }
 
     }
+    
 
     @FXML
     private void switchToSecondary() throws IOException {
